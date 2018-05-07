d0e46356c372feeae23e72e08c06e72f2015aca5
xaipo <xaipoj@hotmail.com>
2018-05-06 22:21:35 -0500

    cambio en notificaciones

--- Client/angell/src/app/AdministratorComponents/dashboard/dashboard.component.ts
+++ Client/angell/src/app/AdministratorComponents/dashboard/dashboard.component.ts
@@ -13,28 +13,9 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     if(this.notification.length>0){
-      this.openNav();
+  
     }
 
   }
-  openNav() {
-    document.getElementById("mySidenav").style.width = "350px";
- //   document.getElementById("main").style.marginLeft = "300px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    this.status=false;
-  }
 
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  //  document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-
-  }
-  expand() {
-    document.getElementById("mySidenav").style.width = "100%";
-    //  document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    this.status=true;
-  }
 }
