ef52321eb5a5690b011c143971f9931a2173006d
jorge.munoz@easysoft.com.ec <mrjleo1989@gmail.com>
2018-11-22 00:23:52 -0500

    cambios jleo

---
+++ Client/angell/src/app/Services/send-email/send-email.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { GLOBAL } from '../global';
+
+@Injectable()
+export class SendEmailService {
+
+  public url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = GLOBAL.url;
+  }
+
+  sendNotifications(mailDaemon) {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(this.url + 'email', mailDaemon, { headers });
+  }
+
+}
