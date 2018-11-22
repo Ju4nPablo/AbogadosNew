199b16c2f58b8b31106bfa646afd2c56a1d8e6f9
xaipo <xaipoj@hotmail.com>
2018-11-22 00:18:24 -0500

    hgvfhjh

--- HttpServices/Models/ModeloMail.js
+++ HttpServices/Models/ModeloMail.js
@@ -4,8 +4,7 @@ const nodemailer = require('nodemailer');
 // Generate test SMTP service account from ethereal.email
 // Only needed if you don't have a real mail account for testing
 module.exports= {
-
-
+    
     sendMail: function (datos, callback) {
         nodemailer.createTestAccount((err, account) => {
             // create reusable transporter object using the default SMTP transport
